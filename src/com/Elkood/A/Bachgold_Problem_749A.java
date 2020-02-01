44fe1b25f951178ca6d1dfee90976b7b1b713148
Abdalqader27 <46373332+Abdalqader27@users.noreply.github.com>
2020-02-01 22:57:21 +0200

    push 1 problems

--- src/com/Elkood/A/Bachgold_Problem_749A.java
+++ src/com/Elkood/A/Bachgold_Problem_749A.java
@@ -1,27 +1,27 @@
-package com.Elkood;
+package com.Elkood.A;
+
+import com.Elkood.Main;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.lang.reflect.Array;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
 import java.util.stream.IntStream;
 
-
-public class Main {
+public class Bachgold_Problem_749A {
     static int n;
 
 
     public static void main(String[] args) {
         PrintWriter pw = new PrintWriter(System.out);
-        Input(new FastReader(), pw);
+        Input(new Main.FastReader(), pw);
         pw.flush();
         pw.close();
     }
 
-    public static void Input(FastReader input, PrintWriter pw) {
+    public static void Input(Main.FastReader input, PrintWriter pw) {
         n = input.nextInt();
         ArrayList<Integer> ll = new ArrayList<>();
         while (n > 0) {
